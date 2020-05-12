59b4fe1801b056c018510e54de159baa831eff6e
Pablo Rivera <pabloriverar.sv@gmail.com>
2020-05-12 10:08:47 -0600

    actualizacion 05-12-20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+ nombre:string = 'Capitan America';
+
+ arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+ PI:number = Math.PI;
+
+ porcentaje: number = 0.234;
+
+ Salario:number = 1234.5;
+
 }
